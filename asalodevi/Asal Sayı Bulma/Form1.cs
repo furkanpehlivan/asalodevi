425d6d6cad6ae3a831784099ea9323b5803a1a09
furkanpehlivan <116825105+furkanpehlivan@users.noreply.github.com>
2022-11-16 19:20:42 +0300

    Asal sayı bulma ödevi

---
+++ asalodevi/Asal Sayı Bulma/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asal_Sayı_Bulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayı1=Convert.ToInt64(textBox1.Text);
+            double sayı2=Convert.ToInt64(textBox2.Text);
+            for (double i=sayı1;i<=sayı2 ;i++)
+            {
+                int kontrol = 0;
+                for(int j=2;j<i;j++)
+                {
+                    if (i%j==0)
+                    {
+                        kontrol++;
+                        break;
+                    }
+                }
+                if (kontrol==0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+           
+            
+            }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+        }
+    
+
